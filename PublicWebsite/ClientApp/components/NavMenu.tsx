a00246fc5d091226be6bafee982ff31f1638854c
Victor Foster <victor.foster@gmail.com>
2018-02-19 10:43:02 -0700

    Updated public site to use aspnet core react spa template

--- PublicWebsite/ClientApp/components/NavMenu.tsx
+++ PublicWebsite/ClientApp/components/NavMenu.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export class NavMenu extends React.Component<{}, {}> {
     public render() {
@@ -18,7 +18,7 @@ export class NavMenu extends React.Component<{}, {}> {
                 <div className='navbar-collapse collapse'>
                     <ul className='nav navbar-nav'>
                         <li>
-                            <NavLink exact to={ '/' } activeClassName='active'>
+                            <NavLink to={ '/' } exact activeClassName='active'>
                                 <span className='glyphicon glyphicon-home'></span> Home
                             </NavLink>
                         </li>
